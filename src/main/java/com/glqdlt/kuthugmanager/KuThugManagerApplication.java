a287809a2ef10d6d697116f1df85237baff320ae
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-04-06 15:14:53 +0900

    fixme : rollback 전

--- src/main/java/com/glqdlt/kuthugmanager/KuThugManagerApplication.java
+++ src/main/java/com/glqdlt/kuthugmanager/KuThugManagerApplication.java
@@ -1,23 +1,72 @@
 package com.glqdlt.kuthugmanager;
 
+import com.opencsv.CSVReader;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+@EnableAsync
+@EnableScheduling
+@Slf4j
 @SpringBootApplication
 public class KuThugManagerApplication implements CommandLineRunner {
 
-	@Autowired
-	SimpleThreadPools simpleThreadPools;
+
+    @Value("${thug.agent.url}")
+    private String[] THUG_AGENTS;
+
+    @Value("${url.csv.path}")
+    private String URL_PATH;
+
+    List<String[]> urlList;
+
+    @Autowired
+    SimpleThreadPools simpleThreadPools;
+
+    @PostConstruct
+    private void init() {
+
+        try (Reader reader = Files.newBufferedReader(Paths.get(URL_PATH));
+             CSVReader csvReader = new CSVReader(reader)) {
+            urlList = csvReader.readAll();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+
+        }
+    }
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(KuThugManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KuThugManagerApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		simpleThreadPools.runner();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        batcher();
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    private void batcher(){
+        int agentIndex = 0;
+        for (String url[] : urlList) {
+            if (agentIndex >= THUG_AGENTS.length) {
+                agentIndex = 0;
+            }
+            simpleThreadPools.run(url[0], THUG_AGENTS[agentIndex]);
+            agentIndex++;
+        }
+    }
 }
--- src/main/java/com/glqdlt/kuthugmanager/SimpleThreadPools.java
+++ src/main/java/com/glqdlt/kuthugmanager/SimpleThreadPools.java
@@ -3,7 +3,13 @@ package com.glqdlt.kuthugmanager;
 import com.opencsv.CSVReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -11,53 +17,46 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.rmi.server.ObjID;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * Created By iw.jhun
  * On 2018-03-29 , 오후 4:53
  */
 
-@Component
+@Service
 @Slf4j
 public class SimpleThreadPools {
 
     @Value("${thug.agent.url}")
     private String[] THUG_AGENTS;
 
-    @Value("${url.csv.path}")
-    private String URL_PATH;
 
-    List<String[]> urlList;
+    @Bean
+    public Executor asyncExecutor(){
 
-    @PostConstruct
-    private void init() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setMaxPoolSize(THUG_AGENTS.length);
+        threadPoolTaskExecutor.setThreadNamePrefix("ThugAgent-");
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
 
-        try (Reader reader = Files.newBufferedReader(Paths.get(URL_PATH));
-             CSVReader csvReader = new CSVReader(reader)) {
-            urlList = csvReader.readAll();
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-
-        }
     }
 
-    public void runner() {
-        ExecutorService executor = Executors.newFixedThreadPool(THUG_AGENTS.length);
-        for(int i=0; i<999;i++) {
-            int agentIndex = 0;
-            for (String url[] : urlList) {
-                if (agentIndex >= THUG_AGENTS.length) {
-                    agentIndex = 0;
-                }
-                executor.submit(new JobWorker(url[0], THUG_AGENTS[agentIndex]));
-                agentIndex++;
-            }
+    @Async
+    public void run(String url, String thugAgent){
+        log.info("[start] thug_agent : '{}' url : '{}'", thugAgent, url);
+        Map<String, String> map = new HashMap<>();
+        map.put("url", url);
+//                    restTemplate.postForObject("http://127.0.0.1:15000/thug/remote/",map,String.class);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
+        log.info("[end] thug_agent : '{}' url : '{}'", thugAgent, url);
     }
 }
