79dc2d7f2a8d20d461986dc3fe4b88bf158f8a94
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-04-06 13:19:42 +0900

    improve : csv 파일로 url 읽어오는 것 추가

--- src/main/java/com/glqdlt/kuthugmanager/SimpleThreadPools.java
+++ src/main/java/com/glqdlt/kuthugmanager/SimpleThreadPools.java
@@ -1,15 +1,16 @@
 package com.glqdlt.kuthugmanager;
 
-import lombok.extern.log4j.Log4j;
+import com.opencsv.CSVReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,37 +26,41 @@ public class SimpleThreadPools {
     @Value("${thug.agent.url}")
     private String[] THUG_AGENTS;
 
+    @Value("${url.csv.path}")
+    private String URL_PATH;
+
+    List<String[]> urlList;
+
+    @PostConstruct
+    private void init() throws IOException {
+
+        Reader reader
+                = Files.newBufferedReader(Paths.get(URL_PATH));
+        CSVReader csvReader = new CSVReader(reader);
+        urlList = csvReader.readAll();
+    }
+
     public void runner() {
-
-
-        // TODO csv 파일을 읽어와서 콜렉션으로 return 하는 기능 개발 필요
-        List<String> list = new ArrayList<>();
-//        list.add("http://www.google.co.kr");
-//        list.add("http://www.daum.net");
-//        list.add("http://www.naver.com");
-//        list.add("http://www.nate.com");
-
-        for(int i =0; i<1000; i++){
-            list.add("http://www.asdasd.00"+i);
-        }
-
 
 
         ExecutorService executor = Executors.newFixedThreadPool(THUG_AGENTS.length);
 
-        int agentNumb = 0;
+        int agentIndex = 0;
+        int urlIndex = 0;
 
         while (true) {
 
-            if(agentNumb >= THUG_AGENTS.length){
-                agentNumb = 0;
+            if (agentIndex >= THUG_AGENTS.length) {
+                agentIndex = 0;
+            }
+            if (urlIndex >= urlList.size()) {
+                urlIndex = 0;
             }
 
-            for (String url : list) {
-                executor.execute(new JobWorker(url,THUG_AGENTS[agentNumb]));
-            }
+            executor.execute(new JobWorker(urlList.get(urlIndex)[0], THUG_AGENTS[agentIndex]));
 
-            agentNumb++;
+            agentIndex++;
+            urlIndex++;
         }
     }
 
---
+++ src/test/java/com/glqdlt/kuthugmanager/TestCsvRead.java
@@ -0,0 +1,53 @@
+package com.glqdlt.kuthugmanager;
+
+import com.opencsv.CSVReader;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.applet.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Created By iw.jhun
+ * On 2018-04-06 , 오전 11:44
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestCsvRead {
+    @Value("${url.csv.path}")
+    private String URL_PATH;
+
+
+//    @Test
+    public void makeFile() throws IOException {
+        String path = "C:\\Users\\iw.jhun\\Documents\\workspace-idea\\ku-thug-manager\\src\\main\\resources";
+        for(int i=0; i<9999;i++){
+            String text = "http://"+i+System.lineSeparator();
+            Files.write(Paths.get(path,"test.csv"),text.getBytes(), StandardOpenOption.CREATE,StandardOpenOption.APPEND);
+        }
+    }
+
+    @Test
+    public void test() {
+        try {
+            Reader reader
+                    = Files.newBufferedReader(Paths.get(URL_PATH));
+            CSVReader csvReader = new CSVReader(reader);
+            csvReader.readAll().forEach(x -> log.info(x[0]));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
